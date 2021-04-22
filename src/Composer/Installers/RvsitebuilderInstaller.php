abe6e8e685099908b3bb4ab7b1f157771fd379f0
Arnut Klamhui <45161973+klaarnut@users.noreply.github.com>
2021-04-22 18:05:39 +0700

    Update RvsitebuilderInstaller.php

--- src/Composer/Installers/RvsitebuilderInstaller.php
+++ src/Composer/Installers/RvsitebuilderInstaller.php
@@ -4,6 +4,7 @@ namespace Composer\Installers;
 class RvsitebuilderInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'package'    => 'packages/{$vendor}/{$name}/',        
+        'package'    => 'packages/{$vendor}/{$name}/',
+        'app'    => 'packages/{$vendor}/{$name}/', 
     );
 }
